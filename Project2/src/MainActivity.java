8f12d1bf43f6d81fc5c076a9b4c8dc3be47fb475
djuarez <djuarez@w10w145late5500.amazonprocessing.com>
2018-02-06 13:53:46 -0800

    Apply auto-format

--- Project2/src/MainActivity.java
+++ Project2/src/MainActivity.java
@@ -1,23 +1,22 @@
 
 public class MainActivity {
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 
-	int n;
-	double x;
+		int n;
+		double x;
 
-	n = 10;
-	x = 6.5;
+		n = 10;
+		x = 6.5;
 
-	System.out.println("Hello there!");
-	System.out.println(n);
-	System.out.println("x is " + x);
+		System.out.println("Hello there!");
+		System.out.println(n);
+		System.out.println("x is " + x);
 
-	int z;
-	z = 6;
-	System.out.println("z " + z + "!!!");
+		int z;
+		z = 6;
+		System.out.println("z " + z + "!!!");
 
-	int y = 100;
+		int y = 100;
 
 	}
 }
