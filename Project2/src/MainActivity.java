cb6ae434027dca097465ade2a07322bfb9ebcc0a
djuarez <djuarez@w10w145late5500.amazonprocessing.com>
2018-02-06 13:51:45 -0800

    Project 2 initial commit

---
+++ Project2/src/MainActivity.java
@@ -0,0 +1,23 @@
+
+public class MainActivity {
+	public static void main(String args[])
+	{
+
+	int n;
+	double x;
+
+	n = 10;
+	x = 6.5;
+
+	System.out.println("Hello there!");
+	System.out.println(n);
+	System.out.println("x is " + x);
+
+	int z;
+	z = 6;
+	System.out.println("z " + z + "!!!");
+
+	int y = 100;
+
+	}
+}
