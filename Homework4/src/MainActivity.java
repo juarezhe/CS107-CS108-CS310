9fb9bf8c1f2dede18eedb59fd5062e78e4115a13
djuarez <djuarez@w10w145late5500.mshome.net>
2018-02-20 15:56:24 -0800

    Initial (and final) commit.

---
+++ Homework4/src/MainActivity.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class MainActivity {
+	public static void main(String args[]) {
+		Scanner reader = new Scanner(System.in);
+		String username;
+		byte tenDollarTicketCount;
+		byte twentyDollarTicketCount;
+		
+		System.out.println("Welcome to homework 4.");
+		System.out.println("Homework 4 is a highly advanced ticket-ordering software.");
+		System.out.print("User, please enter your name: ");
+		username = reader.nextLine();
+		
+		System.out.print("\nHow many $10 tickets would you like? ");
+		tenDollarTicketCount = reader.nextByte();
+		while (tenDollarTicketCount < 0) {
+			System.out.println("\nUnfortunately, we aren't able to purchase your tickets at the moment.");
+			System.out.print("How many $10 tickets would you like? ");
+			tenDollarTicketCount = reader.nextByte();
+		}
+		
+		System.out.print("\nHow many $20 tickets would you like? ");
+		twentyDollarTicketCount = reader.nextByte();
+		while (twentyDollarTicketCount < 0) {
+			System.out.println("\nUnfortunately, we aren't able to purchase your tickets at the moment.");
+			System.out.print("How many $10 tickets would you like? ");
+			twentyDollarTicketCount = reader.nextByte();
+		}
+		
+		System.out.println("\nThank you for your order, " + username + ".");
+		System.out.print("For (" + tenDollarTicketCount + ") $10 ticket");
+		if(tenDollarTicketCount != 1) {
+			System.out.print("s");
+		}
+		System.out.print(" and (" + twentyDollarTicketCount + ") $20 ticket");
+		if(twentyDollarTicketCount != 1) {
+			System.out.print("s");
+		}
+		System.out.print(", that will be $" + (10 * tenDollarTicketCount + 20 * twentyDollarTicketCount) + " total.");
+	}
+}
