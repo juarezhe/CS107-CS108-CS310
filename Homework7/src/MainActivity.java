7b3185bd3e22f9bc5d912e3df4b0e9e6ec6b9a48
djuarez <23268165+whyrll@users.noreply.github.com>
2018-04-06 16:30:27 -0700

    Request integers from user between -1, 100; Display sum and count; Display average when user enters -1

--- Homework7/src/MainActivity.java
+++ Homework7/src/MainActivity.java
@@ -1,4 +1,35 @@
+import java.util.Scanner;
 
 public class MainActivity {
+	
+	private static Scanner scan;
 
+	public static void main(String args[]) {
+		int count = 0;
+		int sum = 0;
+		int value = 0;
+		
+		scan = new Scanner(System.in);
+		
+		System.out.println("Please input an integer from 0 to 100, or -1 to quit.");
+		value = Integer.parseInt(scan.nextLine());
+		
+		do {
+			if (value < -1 || value > 100) {
+				System.out.println("Sorry, the expected range is -1 through 100. Please try again.");
+				value = Integer.parseInt(scan.nextLine());
+			}
+			else {
+				sum += value;
+				count++;
+				System.out.println("Sum = " + sum + ", Count = " + count + "\n");
+				System.out.println("Please input an integer from 0 to 100, or -1 to quit.");
+				value = Integer.parseInt(scan.nextLine());
+			}
+		} while (value != -1);
+		
+		float average = (float) sum / count;
+		
+		System.out.println("The average of the values entered is " + average + ".");
+	}
 }
