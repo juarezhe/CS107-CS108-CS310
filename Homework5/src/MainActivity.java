ff9d919c3dd0c8bcf5f710e0b617d33020c5b98b
djuarez <23268165+whyrll@users.noreply.github.com>
2018-04-06 14:24:18 -0700

    Change to count up on x and y axis until max area is reached and perimeter is less than 3000

--- Homework5/src/MainActivity.java
+++ Homework5/src/MainActivity.java
@@ -1,30 +1,28 @@
 import java.util.Scanner;
 
 public class MainActivity {
-	
-	private static Scanner scan;
-
 
 	public static void main(String args[]) {
-		int perimeter, length, subLength, lengthDivisions, width, subWidth, widthDivisions, area;
+		int perimeter = 3000;
+		int length = 0;
+		int lSegments = 3;
+		int width = 0;
+		int wSegments = 4;
+		int area = 0;
+
+		for (int x = 1; x * lSegments < perimeter; x++) {
+			for (int y = 1; y * wSegments < perimeter - x; y++) {
+				int tempArea = x * lSegments * y * wSegments;
+				
+				if (tempArea > area && x * lSegments + y * wSegments <= perimeter) {
+					area = tempArea;
+					length = x;
+					width = y;
+				}
+			}
+		}
 		
-		scan = new Scanner(System.in);
-		
-		System.out.println("What is the maximum fence length?");
-		perimeter = Integer.parseInt(scan.nextLine());
-		
-		System.out.println("How many times will the area be split along its width?");
-		widthDivisions = Integer.parseInt(scan.nextLine());
-		
-		System.out.println("How many times will the area be split along its length?");
-		lengthDivisions = Integer.parseInt(scan.nextLine());
-		
-		length = width = perimeter / 4;
-		subLength = length / lengthDivisions;
-		subWidth = width / widthDivisions;
-		area = length * width;
-		
-		System.out.println("The optimal area of " + area + " is obtained with a length of " + length + " and a  width of " + width + ".");
-		System.out.println("For each subdivision, the length will be " + subLength + " and the width will be " + subWidth + ".");
+		System.out.println("The optimal area of " + area + " is obtained with a length of " + length * lSegments + " and a  width of " + width * wSegments + ".");
+		System.out.println("For each subdivision, the length will be " + length + " and the width will be " + width + ".");
 	}
 }
