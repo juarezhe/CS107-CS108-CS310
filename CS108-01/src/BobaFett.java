61309c840bb9ce69bf3f4fb3e2a08b03e6f6a668
djuarez <23268165+whyrll@users.noreply.github.com>
2018-09-10 13:29:02 -0700

    Create BobaFett class that implements GameCharacter, initialize instance variables

---
+++ CS108-01/src/BobaFett.java
@@ -0,0 +1,19 @@
+
+public class BobaFett implements GameCharacter {
+	
+	double x, y; // x and y coordinates
+	double h, w; // height and width (for nearness to screen)
+
+	@Override
+	public void drawOnScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void makeSound() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
