598d2427316f09d146c6adae2f24002f03f083dd
djuarez <23268165+whyrll@users.noreply.github.com>
2018-09-19 14:12:46 -0700

    Update code to reflect solution

--- CS108-02/src/ArrayStack.java
+++ CS108-02/src/ArrayStack.java
@@ -3,57 +3,67 @@ import java.util.ArrayList;
 public class ArrayStack {
 	
 	//private Object[] btsarr = new Object[100];
-	private ArrayList<Object> btsarr = new ArrayList<Object>(0);
+	//private ArrayList<Object> btsarr = new ArrayList<Object>(0);
+	private ArrayList<Object> btsarr;
 	private int topidx;
 	
 	public ArrayStack() {
-		topidx = -1;
+		//this.btsarr = new Object[100];
+		this.btsarr = new ArrayList<Object>(0);
+		this.topidx = -1;
 	}
 	
 	public void push(Object x) {
-		topidx++;
-		//btsarr[topidx] = x;
-		btsarr.add(x);
+		if (this.size() ==  100) {
+			System.out.println("Out of space!");
+		}
+		else {
+			this.topidx++;
+			//this.btsarr[this.topidx] = x;
+			this.btsarr.add(x);
+		}
 	}
 	
 	public Object pop() {
-		if (topidx == -1) {
+		//if (this.topidx == -1) {
+		if (this.size() == 0) {
 			System.out.println("Out of items!");
 			return null;
 		}
 		else {
-			Object objectToReturn = btsarr.get(topidx);
-			btsarr.remove(topidx);
-			topidx--;
-			//return btsarr[topidx + 1];
+			//Object objectToReturn = this.btsarr[this.topidx];
+			Object objectToReturn = this.btsarr.get(this.topidx);
+			//this.btsarr[this.topidx] = null;
+			this.btsarr.remove(this.topidx);
+			this.topidx--;
 			return objectToReturn;
 		}
 	}
 	
 	public int size() {
-		//return topidx + 1;
-		return btsarr.size();
+		//return this.topidx + 1;
+		return this.btsarr.size();
 	}
 	
 	public boolean isEmpty() {
-		if (topidx == -1) {
+		/**
+		if (this.topidx == -1) {
 			return true;
 		}
 		else {
 			return false;
-		}
+		}**/
+		return this.topidx == -1;
 	}
 	
 	@Override
 	public String toString() {
 		if (!isEmpty()) {
-			String stringToReturn;
+			String stringToReturn = "top\n";
 			
-			stringToReturn = "top\n";
-			
-			for (int i = topidx; i > -1; i--) {
-				//stringToReturn += btsarr[i] + "\n";
-				stringToReturn += btsarr.get(i) + "\n";
+			for (int i = this.topidx; i > -1; i--) {
+				//stringToReturn += this.btsarr[i] + "\n";
+				stringToReturn += this.btsarr.get(i) + "\n";
 			}
 			
 			stringToReturn += "bottom\n";
